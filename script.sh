dc63a02755245c1c5f23a600bf39517e7e958aa4
Frostoriginal <69118669+Frostoriginal@users.noreply.github.com>
2025-07-30 12:34:15 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -152,7 +152,7 @@ dpkg -s mssql-tools18 &> /dev/null
 		curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
 		curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 		sudo apt-get update
-		sudo apt-get install mssql-tools18 unixodbc-dev
+		sudo apt-get install mssql-tools18 unixodbc-dev -y
 		sudo apt-get update
 		sudo apt-get install mssql-tools18 -y
 		echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bash_profile
